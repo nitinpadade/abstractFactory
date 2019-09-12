c89933bb1083d95f33153eb8ac08e7de23cc2b96
Nitin Padade <nitin.padade@outlook.com>
2019-09-12 16:29:29 +0100

    Abstract Factory Pattern

---
+++ AbstractLayer/Parameter/UserParameter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractLayer.Parameter
+{
+    public class UserParameter
+    {
+        public int Id { get; set; }
+
+    }
+}
---
+++ AbstractLayer/Query/UserQuery.cs
@@ -0,0 +1,13 @@
+﻿using AbstractLayer.Model;
+using AbstractLayer.Parameter;
+
+namespace AbstractLayer.Query
+{
+    public class UserQuery : IQuery<User, UserParameter>
+    {
+        public User Execute(UserParameter parameters)
+        {
+            return new User { Id = 1, Name = "Devansh N Padade", Message = parameters.Id.ToString() };
+        }
+    }
+}
